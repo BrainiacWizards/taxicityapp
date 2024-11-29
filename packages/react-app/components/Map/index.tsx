f735f8570506ad5821e0d4c88d2c61e50004fe88
mpotulom28 <mpotulom28@gmail.com>
2024-11-29 19:01:26 +0200

    feat: implement current location tracking in GoogleMap component with fallback to first location

--- packages/react-app/components/Map/index.tsx
+++ packages/react-app/components/Map/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   AdvancedMarker,
   APIProvider,
@@ -11,18 +11,33 @@ import styles from './map.module.css';
 import { iRank } from '@/models/RankMapModels';
 
 const GoogleMap: React.FC<{ locations: iRank[] }> = ({ locations }) => {
-  const [currentLocation, setCurrentLocation] = React.useState({
-    lat: 0,
-    lng: 0,
-  });
+  const [currentLocation, setCurrentLocation] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
 
-  // get users current location
-  navigator.geolocation.getCurrentPosition((position) => {
-    setCurrentLocation({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
-    });
-  });
+  useEffect(() => {
+    // get users current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      () => {
+        // If user denies permission, use the first location in the locations array
+        if (locations.length > 0) {
+          setCurrentLocation({
+            lat: locations[0].latitude,
+            lng: locations[0].longitude,
+          });
+        }
+      }
+    );
+  }, [locations]);
+
+  console.log('current loc', currentLocation);
 
   const PoiMarkers = (props: { pois: iRank[] }) => {
     return (
@@ -54,17 +69,19 @@ const GoogleMap: React.FC<{ locations: iRank[] }> = ({ locations }) => {
       apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
       onLoad={() => console.log('Maps API has loaded.')}
     >
-      <Map
-        mapId={'609184b6d45dfcf6'}
-        className={styles.map}
-        defaultCenter={currentLocation}
-        colorScheme="dark"
-        defaultZoom={10}
-        gestureHandling={'greedy'}
-        disableDefaultUI={true}
-      >
-        <PoiMarkers pois={locations} />
-      </Map>
+      {currentLocation && (
+        <Map
+          mapId={'609184b6d45dfcf6'}
+          className={styles.map}
+          defaultCenter={currentLocation}
+          colorScheme="dark"
+          defaultZoom={10}
+          gestureHandling={'greedy'}
+          disableDefaultUI={true}
+        >
+          <PoiMarkers pois={locations} />
+        </Map>
+      )}
     </APIProvider>
   );
 };
